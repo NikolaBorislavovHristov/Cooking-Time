5a05bc533e2a5cbe865f0440bf0da1224893032f
Nikola Hristov <anarhisthristov@gmail.com>
2016-01-31 08:46:58 +0200

    Initial commit

---
+++ Cooking Time/Cooking Time/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cooking Time
+//
+//  Created by Nikola Hristov on 30/1/16.
+//  Copyright © 2016 Nikola Hristov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
